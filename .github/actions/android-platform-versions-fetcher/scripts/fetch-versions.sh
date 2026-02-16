76511b19183e54bcba6e79c436ae383f4ca6aa0e
Thomas Wirth <thomas-w-@web.de>
2026-02-17 00:07:11 +0100

    refactor: rename max major versions param (#3)

--- .github/actions/android-platform-versions-fetcher/scripts/fetch-versions.sh
+++ .github/actions/android-platform-versions-fetcher/scripts/fetch-versions.sh
@@ -3,14 +3,14 @@ set -e
 set -o pipefail
 
 max_version=$1
-max_majors=$2
+max_major_versions_count=$2
 xml_url="https://dl.google.com/android/repository/repository2-3.xml"
 
 # Fetch Android platform versions extracting only the version numbers and sorting them in descending order.
 all_versions=$(curl -fsSL "$xml_url" | grep -oP '(?<=platforms;android-)[0-9.]+(?=")' | sort -uVr)
 
-# Get latest max_majors API levels starting at max_version, including all respective minor versions.
-filtered_versions=$(echo "$all_versions" | awk -v start_ver="$max_version" -v max_m="$max_majors" -F. '
+# Get latest max_major_versions_count API levels starting at max_version, including all respective minor versions.
+filtered_versions=$(echo "$all_versions" | awk -v start_ver="$max_version" -v max_m="$max_major_versions_count" -F. '
     BEGIN { major_count = 0; last_major = ""; started = 0 }
     {
         current_major = $1
@@ -32,8 +32,8 @@ filtered_versions=$(echo "$all_versions" | awk -v start_ver="$max_version" -v ma
 ')
 
 # Validate result
-if [ "$(echo "$filtered_versions" | cut -d. -f1 | uniq | grep -c '^')" -lt "$max_majors" ]; then
-    echo "ERROR: Not enough distinct major versions ($max_majors) found for $max_version:" >&2
+if [ "$(echo "$filtered_versions" | cut -d. -f1 | uniq | grep -c '^')" -lt "$max_major_versions_count" ]; then
+    echo "ERROR: Not enough distinct major versions ($max_major_versions_count) found for $max_version:" >&2
     echo "$filtered_versions" >&2
     exit 1
 fi
