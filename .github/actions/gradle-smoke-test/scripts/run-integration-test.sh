9b7cf149c91f2e23357721260c4d85fff841181e
Thomas Wirth <thomas-w-@web.de>
2026-02-21 17:13:45 +0100

    fix: reformat and fix parameter names

--- .github/actions/gradle-smoke-test/scripts/run-integration-test.sh
+++ .github/actions/gradle-smoke-test/scripts/run-integration-test.sh
@@ -9,11 +9,11 @@ action_path=$4
 echo "### Host: Starting Docker Integration Test ###"
 
 docker run --rm \
-  --user "$run_as_uid:$run_as_gid" \
-  -v "$(pwd)":/workspace \
-  -v "$action_path/scripts":/scripts:ro \
-  -w /workspace \
-  "$image" \
-  bash /scripts/gradle-init-and-exec.sh
+    --user "$run_as_uid:$run_as_gid" \
+    -v "$(pwd)":/workspace \
+    -v "$action_path/scripts":/scripts:ro \
+    -w /workspace \
+    "$image" \
+    bash /scripts/gradle-init-and-exec.sh
 
 echo "### Host: Integration Test Finished Successfully ###"
