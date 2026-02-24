01b7c99a99c436f80387c3b344c768c7dfddf6f2
Thomas Wirth <thomas-w-@web.de>
2026-02-24 22:24:25 +0100

    ci: optimize manifest generation output

--- .github/actions/generate-manifest/scripts/generate-manifest.sh
+++ .github/actions/generate-manifest/scripts/generate-manifest.sh
@@ -8,12 +8,14 @@ report_file=$4
 
 get_sdk_components() {
     docker run --rm "$image" sdkmanager --list_installed 2>/dev/null |
-        awk -F '|' '/^[[:space:]]+[a-zA-Z0-9.;_-]+[[:space:]]+\|/ {
-        gsub(/^[[:space:]]+|[[:space:]]+$/, "", $1);
-        gsub(/^[[:space:]]+|[[:space:]]+$/, "", $2);
-        gsub(/^[[:space:]]+|[[:space:]]+$/, "", $3);
-        printf "| %s | %s | %s |\n", $1, $2, $3
-    }' | sort
+        awk -F '|' '
+        /^[[:space:]]*(Path|---)/ { next }
+        /^[[:space:]]+[a-zA-Z0-9.;_-]+[[:space:]]+\|/ {
+            gsub(/^[[:space:]]+|[[:space:]]+$/, "", $1);
+            gsub(/^[[:space:]]+|[[:space:]]+$/, "", $2);
+            gsub(/^[[:space:]]+|[[:space:]]+$/, "", $3);
+            printf "| %s | %s | %s |\n", $1, $2, $3
+        }' | sort
 }
 
 get_apk_packages() {
@@ -24,12 +26,31 @@ get_apk_packages() {
             full=$0; pkg=$0;
             sub(/-[0-9].*$/, "", pkg);
             ver=substr(full, length(pkg) + 2);
-            printf "| %s | %s |\n", pkg, ver
+
+            split(pkg, parts, "-");
+            base=parts[1];
+
+            if (base in last_base && ver == version[base]) {
+                if (index(packages[base], pkg) == 0) {
+                    packages[base] = packages[base] ", " pkg
+                }
+            } else {
+                base_list[++count] = base;
+                packages[base] = pkg;
+                version[base] = ver;
+                last_base[base] = base;
+            }
+        }
+        END {
+            for (i=1; i<=count; i++) {
+                b = base_list[i];
+                printf "| %s | %s |\n", packages[b], version[b]
+            }
         }' | sort
 }
 
 generate_markdown_body() {
-    echo "<details><summary><b>📦 Build Configuration: JDK $jdk_version | Android API $android_api</b></summary>"
+    echo "<details><summary><b>🐳 Docker Image Content: JDK ${jdk_version#jdk} | Android API $android_api</b></summary>"
     echo ""
     echo "#### 🤖 Android SDK Components"
     echo "| Component | Version | Description |"
@@ -45,8 +66,8 @@ generate_markdown_body() {
     get_apk_packages
 
     echo ""
-    echo "</details>" # End APK details
-    echo "</details>" # End Matrix-Slot details
+    echo "</details>"
+    echo "</details>"
     echo ""
 }
 
