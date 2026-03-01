fb03bc17c16a397434ec144ca459aa9449b25142
Thomas Wirth <thomas-w-@web.de>
2026-03-01 17:56:11 +0100

    ci: change to standard npx command format

--- .github/actions/renovate-config-validator/scripts/run-renovate-config-validator.sh
+++ .github/actions/renovate-config-validator/scripts/run-renovate-config-validator.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 config_file=$1
 
-npx --yes --loglevel error --package renovate@latest --call "renovate-config-validator $config_file"
+npx --yes --loglevel error --package renovate@latest renovate-config-validator "$config_file"
